6f6201c3f489e2498e91847205271133f18265b7
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-06-19 23:38:13 +0300

    Add background and styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,77 +7,96 @@ function Book(title, author, pages, read = false) {
 
 const myLibrary = [];
 
-myLibrary.push(new Book('The Call of The Wild', 'Jack London', '80'));
-myLibrary.push(new Book('Hitchiker\'s Guide to The Galaxy', 'Douglas Adams', '250', true));
+myLibrary.push(new Book("The Call of The Wild", "Jack London", "80"));
+myLibrary.push(
+  new Book("Hitchiker's Guide to The Galaxy", "Douglas Adams", "250", true)
+);
 
-const library = document.getElementById('library');
+const library = document.getElementById("library");
 function render() {
-  myLibrary.forEach(item => {
-    const row = document.createElement('tr');
-    const bookTitle = document.createElement('td');
+  myLibrary.forEach((item) => {
+    const row = document.createElement("tr");
+
+    const bookId = document.createElement("th");
+    bookId.scope = "row";
+    bookId.textContent = myLibrary.indexOf(item) + 1;
+    const bookTitle = document.createElement("td");
     bookTitle.textContent = item.title;
-    const bookAuthor = document.createElement('td');
+    const bookAuthor = document.createElement("td");
     bookAuthor.textContent = item.author;
-    const bookPages = document.createElement('td');
+    const bookPages = document.createElement("td");
     bookPages.textContent = item.pages;
-    const bookRead = document.createElement('td');
+    const bookRead = document.createElement("td");
     bookRead.textContent = item.read;
 
+    const bookRemove = document.createElement("td");
+    bookRemove.textContent = "remove?";
+
+    row.appendChild(bookId);
     row.appendChild(bookTitle);
     row.appendChild(bookAuthor);
     row.appendChild(bookPages);
     row.appendChild(bookRead);
+    row.appendChild(bookRemove);
     library.appendChild(row);
   });
 }
 render();
 
-const addBookSectionButton = document.getElementById('addBookSectionButton');
+const addBookSectionButton = document.getElementById("addBookSectionButton");
 
-
-const addBookSection = document.getElementById('addBookSection');
+const addBookSection = document.getElementById("addBookSection");
 
 function showAddSection() {
-  if (addBookSectionButton.innerHTML === 'CLOSE ADD BOOK SECTION') {
-    addBookSection.style.display = 'none';
-    addBookSectionButton.innerHTML = 'OPEN ADD BOOK SECTION';
+  if (addBookSectionButton.innerHTML === "CLOSE ADD BOOK SECTION") {
+    addBookSection.style.display = "none";
+    addBookSectionButton.innerHTML = "OPEN ADD BOOK SECTION";
   } else {
-    addBookSection.style.display = 'block';
-    addBookSectionButton.innerHTML = 'CLOSE ADD BOOK SECTION';
+    addBookSection.style.display = "block";
+    addBookSectionButton.innerHTML = "CLOSE ADD BOOK SECTION";
   }
 }
 
-addBookSectionButton.addEventListener('click', showAddSection);
+addBookSectionButton.addEventListener("click", showAddSection);
 
 function addLastBook() {
   const item = myLibrary[myLibrary.length - 1];
-  const row = document.createElement('tr');
-  const bookTitle = document.createElement('td');
+  const row = document.createElement("tr");
+
+  const bookId = document.createElement("th");
+  bookId.scope = "row";
+  bookId.textContent = myLibrary.indexOf(item) + 1;
+  const bookTitle = document.createElement("td");
   bookTitle.textContent = item.title;
-  const bookAuthor = document.createElement('td');
+  const bookAuthor = document.createElement("td");
   bookAuthor.textContent = item.author;
-  const bookPages = document.createElement('td');
+  const bookPages = document.createElement("td");
   bookPages.textContent = item.pages;
-  const bookRead = document.createElement('td');
+  const bookRead = document.createElement("td");
   bookRead.textContent = item.read;
 
+  const bookRemove = document.createElement("td");
+  bookRemove.textContent = "remove?";
+
+  row.appendChild(bookId);
   row.appendChild(bookTitle);
   row.appendChild(bookAuthor);
   row.appendChild(bookPages);
   row.appendChild(bookRead);
+  row.appendChild(bookRemove);
   library.appendChild(row);
 }
 
-
 function addBookToLibrary() {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  const pages = document.getElementById('pages').value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
   const read = document.querySelector('input[name="read"]:checked').value;
 
   myLibrary.push(new Book(title, author, pages, read));
   addLastBook();
+  document.getElementsByTagName("form")[0].reset();
 }
 
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', addBookToLibrary);
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", addBookToLibrary);
