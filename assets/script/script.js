3435b35a7eaabc7a5a0f787b5b5d6d526d216e79
javitocor <56809963+javitocor@users.noreply.github.com>
2020-06-19 16:43:48 +0200

    Add render function

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,10 +5,32 @@ function Book(title, author, pages, read = false) {
   this.read = read;
 }
 
-let myLibrary = [];
+const myLibrary = [];
 
 myLibrary.push(new Book('The Call of The Wild', 'Jack London', '80'));
 myLibrary.push(new Book('Hitchiker\'s Guide to The Galaxy', 'Douglas Adams', '250', true));
 
 function addBookToLibrary(title, author, pages, read) {
 }
+
+const library = document.getElementById('library');
+function render() {
+  myLibrary.forEach(item => {
+    const row = document.createElement('tr');
+    const book_title = document.createElement('td');
+    book_title.textContent = item.title;
+    const book_author = document.createElement('td');
+    book_author.textContent = item.author;
+    const book_pages = document.createElement('td');
+    book_pages.textContent = item.pages;
+    const book_read = document.createElement('td');
+    book_read.textContent = item.read;
+
+    row.appendChild(book_title);
+    row.appendChild(book_author);
+    row.appendChild(book_pages);
+    row.appendChild(book_read);
+    library.appendChild(row);
+  });
+}
+render();
