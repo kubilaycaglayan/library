f43e75bd2c9492c9880f2657ee56a7ef378bac0d
Kubilay <kblycaglayan@gmail.com>
2020-06-19 16:58:26 +0300

    Add form book

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,14 @@
-let javi = {
-  goodness: true
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+let myLibrary = [];
+
+myLibrary.push(new Book('The Call of The Wild', 'Jack London', '80'));
+myLibrary.push(new Book('Hitchiker\'s Guide to The Galaxy', 'Douglas Adams', '250', true));
+
+function addBookToLibrary(title, author, pages, read) {
 }
